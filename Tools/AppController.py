234961fecb3d387474fd26e744cdfd8c479847ff
Roberto C <conigliaro.roberto.mi@gmail.com>
2017-01-08 00:15:22 +0100

    Docker remote working

--- Tools/AppController.py
+++ Tools/AppController.py
@@ -15,7 +15,9 @@ client.connect(Setting.Broker_ip)
 client.loop_start()
 client.on_message = on_message
 #msg={'cmd':'run','appl':'{"app_name":"app2","cpu_quota":"20000","image_name":"test-python"}'}
-msg={'cmd':'update','appl':'{"app_name":"app1","cpu_quota":"30000","image_name":"test-python"}'}
+#msg={'cmd':'regist','appl':'{"app_name":"app1","cpu_quota":"30000","image_name":"test-python"}'}
+
+msg={'cmd':'stop','appl':'{"app_name":"app1","online":"no","boot":"yes","cpu_quota":"10000","image_name":"test-python"}'}
 client.publish("/application/registry/id1",json.dumps(msg),0,retain=False)
 print("Sending .....")
 time.sleep(3)
