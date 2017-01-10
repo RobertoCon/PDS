2f0a8d7bec940f20d6573113eaed308fbe817be6
Roberto C <conigliaro.roberto.mi@gmail.com>
2017-01-10 03:57:05 +0100

    Model fix 2

--- Model/Setting.py
+++ Model/Setting.py
@@ -3,7 +3,6 @@ Created on 26 ott 2016
 
 @author: Conny
 '''
-
 #Node properties:
 node_id="id1"
 node_type="Compute"
@@ -19,23 +18,3 @@ os_version="8.0"
 # Framework properties  
 path="./"
 Broker_ip="192.168.1.6"
-'''
-node_templates:
-      node_id : ID
-      type: Pi-Model
-      capabilities:
-        # Host properties
-        host:
-          properties:
-            num_cpus: 1 
-            disk_size: 14 GB
-            mem_size: 512 MB
-        # Operating System properties
-        os:
-          properties:
-            # Operating System image properties
-            architecture: ARM 
-            type: linux  
-            distribution: jessie  
-            version: 8.0
-'''
