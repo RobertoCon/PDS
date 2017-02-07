c0812903d9fa5d2ce48ff68af11c89dce6be1cd4
Roberto Conigliaro <conigliaro.roberto.mi@gmail.com>
2017-02-07 15:22:08 +0100

    New node_id

--- Model/Setting.py
+++ Model/Setting.py
@@ -6,6 +6,8 @@ Created on 26 ott 2016
 from pathlib import Path
 import yaml
 
+from subprocess import PIPE, run
+
 #Framework
 path="./"
 
@@ -13,9 +15,13 @@ def getNodeId():
     path = Path("./Settings/").absolute()
     path=path.joinpath("NodeRegistry.yaml")
     nodes=yaml.load(open(str(path),'r'))
+    with open("/etc/hostname") as f:
+        host = f.readlines()
+    ip=run("hostname -i", stdout=PIPE, stderr=PIPE, universal_newlines=True)
     for node in nodes['node_templates']:  
-        return node
+        return node+":"+host+":"+ip.stdout
 
+    
 
 def getBrokerIp():
     path = Path("./Settings/").absolute()
@@ -23,3 +29,4 @@ def getBrokerIp():
     nodes=yaml.load(open(str(path),'r'))
     for node in nodes['node_templates']:  
         return nodes['node_templates'][node]['attributes']['broker_address']
+    
