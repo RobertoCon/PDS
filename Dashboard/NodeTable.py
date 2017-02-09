c9a3aa6da6902926a8274e27278bfc1f5d65b9a0
Roberto Conigliaro <conigliaro.roberto.mi@gmail.com>
2017-02-09 17:56:29 +0100

    Dashboard 8

--- Dashboard/NodeTable.py
+++ Dashboard/NodeTable.py
@@ -44,8 +44,8 @@ class NodeTable(object):
             """ % (str(i),nodes['node_templates'][node]['id'],nodes['node_templates'][node]['attributes']['public_address'],\
                    nodes['node_templates'][node]['capabilities']['host']['properties']['num_cpus'],nodes['node_templates'][node]['capabilities']['host']['properties']['mem_size'],\
                    nodes['node_templates'][node]['capabilities']['host']['properties']['disk_size'],nodes['node_templates'][node]['capabilities']['host']['properties']['os']['properties']['architecture'],\
-                   '<form action="remove_node" method="post" name="remove_node_id">\
-                   <button type="submit" value="'+nodes['node_templates'][node]['id']+'" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>\
+                   '<form action="remove_node" method="post">\
+                   <button type="submit" value="'+nodes['node_templates'][node]['id']+'" name="remove_node_id" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-remove" aria-hidden="true"></span></button>\
                    </form>')
         
         return html % code      
