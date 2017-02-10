8d137303a55d77d5f585637f0ba8fdc5a82ef9cc
Roberto Conigliaro <conigliaro.roberto.mi@gmail.com>
2017-02-10 10:21:31 +0100

    Dashboard 3

--- Dashboard/HttpServer.py
+++ Dashboard/HttpServer.py
@@ -62,7 +62,7 @@ class Dashboard(object):
             <br><br><br><br>
             """+NodeTable.getHtml(self.nodes)+"""  
             <form action="add_node" method="post" >
-                   <span class="label label-default"> Hostname:</span><input type="text" name="add_node_id">
+                   <label> Hostname: </label><input type="text" name="add_node_id">
                    <button type="submit" class="btn btn-default btn-lg"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span></button>
             </form> 
             </div><!-- /.container -->
