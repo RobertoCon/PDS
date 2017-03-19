7d04eaaa06d407fbc0a529b67fcfd3be47202902
Roberto Conigliaro <conigliaro.roberto.mi@gmail.com>
2017-03-19 18:23:55 +0100

    Reduce message size 2

--- Device/Factory.py
+++ Device/Factory.py
@@ -20,13 +20,22 @@ class Factory(object):
     @staticmethod
     def decode(serial_dev):
         struct=json.loads(serial_dev)
-        module=load_module("DevicePlugin."+struct['type_dev'])
-        if module!=None:
-            MyClass = getattr(module, struct['type_dev'])
-            instance = MyClass()
-            instance.from_text(serial_dev)
-            return instance
-        return None
+        if type(struct=='dict'):
+            module=load_module("DevicePlugin."+struct['type_dev'])
+            if module!=None:
+                MyClass = getattr(module, struct['type_dev'])
+                instance = MyClass()
+                instance.from_text(serial_dev)
+                return instance
+            return None
+        else:
+            module=load_module("DevicePlugin."+struct[2])
+            if module!=None:
+                MyClass = getattr(module, struct[2])
+                instance = MyClass()
+                instance.from_text(serial_dev)
+                return instance
+            return None
 '''    
     @staticmethod
     def decode_yaml(serial_dev):
