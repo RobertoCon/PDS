d8ff6530430fc9c55d5e213902ca174d3b98d191
Roberto Conigliaro <conigliaro.roberto.mi@gmail.com>
2017-04-19 12:47:50 +0200

    Scenario 3 - 5

--- Benchmark/Scenario3.py
+++ Benchmark/Scenario3.py
@@ -4,15 +4,17 @@ Created on 19 apr 2017
 @author: Conny
 '''
 import cherrypy
+import subprocess
 
 class Scen3(object):
     def __init__(self):
+        self.host=subprocess.getoutput("hostname")
         self.counter=0
    
     @cherrypy.expose
     def index(self):
         self.counter=self.counter+1
-        return str(self.counter)
+        return str(self.counter)+":"+self.host
 
 cherrypy.config.update({'server.socket_host': '0.0.0.0'})
 cherrypy.config.update({'server.socket_port': 9000})
---
+++ Tools/LoadController.py
@@ -0,0 +1,45 @@
+'''
+Created on 19 apr 2017
+
+@author: Conny
+'''
+import paho.mqtt.client as mqtt
+import time,yaml
+  
+client = mqtt.Client()
+client.connect('raspy3-A')
+client.loop_start()
+
+msg='''node_templates:   
+        load1:
+                name: id_1 
+                type: tosca.nodes.LoadBalancer   
+                host: raspy3-A      
+                properties: 
+                    algorithm: RR 
+                    ports:
+                        in_port:
+                            protocol: tcp
+                            target: 8282
+                capabilities:
+                    clients: 
+                        ip_address: 192.168.1.3
+                requirements:
+                    application:
+                        app1:
+                            ip_address: 192.168.1.3 
+                            properties:
+                                ports:
+                                    in_port:
+                                        target: 9000
+                        app2:
+                            ip_address: 192.168.1.6 
+                            properties:
+                                ports:
+                                    in_port:
+                                        target: 9000'''
+
+client.publish("/raspy3-A/model/balancer/add",msg, 0, False)
+print("Sending .....")
+time.sleep(3)
+print("Done")
